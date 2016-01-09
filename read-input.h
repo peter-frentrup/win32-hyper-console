79015cb4e6d015e50e0568ac79056b5f486aaa59
Peter Frentrup <frentrup@math.hu-berlin.de>
2016-01-09 21:22:10 +0100

    document stop_current_input()

--- read-input.h
+++ read-input.h
@@ -7,6 +7,13 @@
  */
 BOOL is_handling_input(void);
 
+/** Tell the current read_input() loop to stop.
+  
+  \param do_abort Whether to return NULL from read_input().
+  \param opt_replace_input An optional replacement of the input buffer before returning.
+  
+  \return Whether there is any a current input loop.
+ */
 BOOL stop_current_input(BOOL do_abort, const wchar_t *opt_replace_input);
 
 /** Read a line of input
