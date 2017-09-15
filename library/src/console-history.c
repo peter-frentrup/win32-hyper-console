1bf98aa25dbcdb55bab80226aadad616a2d80685
Peter Frentrup <frentrup@math.hu-berlin.de>
2017-09-15 22:25:09 +0200

    do not loose current line via history navigation

--- library/src/console-history.c
+++ library/src/console-history.c
@@ -8,13 +8,16 @@
 // TODO: use  text_line_t  and  text_array_t  from scroll-counter.c
 struct history_line_t {
   int length;
-  wchar_t content[1];
+  wchar_t content[1]; // 0-terminated
 };
 
 struct hyper_console_history_t {
   struct history_line_t **entries;
   int capacity;
   int count;
+  
+  wchar_t *future_entry; // 0-terminated
+  int      future_entry_length;
 };
 
 
@@ -25,7 +28,7 @@ struct hyper_console_history_t *hyper_console_history_new(int options) {
   hist = hyper_console_allocate_memory(sizeof(struct hyper_console_history_t));
   if(!hist)
     return NULL;
-  
+    
   memset(hist, 0, sizeof(struct hyper_console_history_t));
   
   return hist;
@@ -43,6 +46,7 @@ void hyper_console_history_free(struct hyper_console_history_t *hist) {
     hyper_console_free_memory(hist->entries[i]);
     
   hyper_console_free_memory(hist->entries);
+  hyper_console_free_memory(hist->future_entry);
   hyper_console_free_memory(hist);
 }
 
@@ -57,7 +61,11 @@ int console_history_count(struct hyper_console_history_t *hist) {
 const wchar_t *console_history_get(struct hyper_console_history_t *hist, int index, int *length) {
   struct history_line_t *line;
   
-  assert(hist != NULL);
+  if(hist == NULL) {
+    if(length)
+      *length = 0;
+    return NULL;
+  }
   
   if(index < 0 || index >= hist->count) {
     if(length != NULL) {
@@ -74,6 +82,55 @@ const wchar_t *console_history_get(struct hyper_console_history_t *hist, int ind
   }
   
   return &line->content[0];
+}
+
+const wchar_t *console_history_get_future(struct hyper_console_history_t *hist, int *length) {
+  if(hist == NULL) {
+    if(length)
+      *length = 0;
+    return NULL;
+  }
+  
+  if(length)
+    *length = hist->future_entry_length;
+    
+  return hist->future_entry;
+}
+
+void console_history_set_future(struct hyper_console_history_t *hist, const wchar_t *text, int text_length) {
+  wchar_t *old_entry;
+  
+  assert(text != NULL || text_length == 0);
+  
+  if(hist == NULL)
+    return;
+    
+  if(text_length < 0) {
+    size_t ulen = wcslen(text);
+    
+    if(ulen >= INT_MAX)
+      return;
+      
+    text_length = (int)ulen;
+  }
+  
+  if(text_length >= INT_MAX / 2)
+    return;
+    
+  old_entry = hist->future_entry;
+  hist->future_entry_length = 0;
+  if(text) {
+    hist->future_entry = hyper_console_allocate_memory((text_length + 1) * sizeof(wchar_t));
+    if(hist->future_entry) {
+      memcpy(hist->future_entry, text, text_length * sizeof(wchar_t));
+      hist->future_entry[text_length] = L'\0';
+      hist->future_entry_length = text_length;
+    }
+  }
+  else 
+    hist->future_entry = NULL;
+  
+  hyper_console_free_memory(old_entry);
 }
 
 void console_history_add(struct hyper_console_history_t *hist, const wchar_t *text, int text_length) {
@@ -97,11 +154,11 @@ void console_history_add(struct hyper_console_history_t *hist, const wchar_t *te
   
   if(text_length >= INT_MAX / 2)
     return;
-  
+    
   // Ignore empty entries
   if(text_length == 0)
     return;
-  
+    
   // Ignore duplicates. TODO: ignore old duplicates if GetConsoleHistoryInfo() says so.
   prev_text = console_history_get(hist, hist->count - 1, &prev_length);
   if( prev_length == text_length &&
@@ -111,10 +168,10 @@ void console_history_add(struct hyper_console_history_t *hist, const wchar_t *te
   }
   
   if(!resize_array(
-      (void**)&hist->entries, 
-      &hist->capacity, 
-      sizeof(hist->entries[0]), 
-      hist->count + 1)) 
+        (void**)&hist->entries,
+        &hist->capacity,
+        sizeof(hist->entries[0]),
+        hist->count + 1))
   {
     return;
   }
@@ -122,7 +179,7 @@ void console_history_add(struct hyper_console_history_t *hist, const wchar_t *te
   line = hyper_console_allocate_memory(sizeof(struct history_line_t) + text_length * sizeof(wchar_t));
   if(line == NULL)
     return;
-  
+    
   line->length = text_length;
   memcpy(&line->content[0], text, text_length * sizeof(wchar_t));
   line->content[text_length] = L'\0';
--- library/src/console-history.h
+++ library/src/console-history.h
@@ -14,13 +14,33 @@ int console_history_count(struct hyper_console_history_t *hist);
 
 /** Get a history entry.
   
-  \param hist A console history buffer.
+  \param hist A console history buffer or NULL.
   \param index The entries index. 0 is the oldest, console_history_count(hist)-1 the newest.
   \param length Optional output parameter where to store the text length.
   
   \return The history entry or NULL on error.
  */
 const wchar_t *console_history_get(struct hyper_console_history_t *hist, int index, int *length);
+
+
+/** Get the supposed next entry (text that is "on hold")
+  
+  \param hist A console history buffer or NULL.
+  \param length Optional output parameter where to store the text length.
+  
+  \return The history future entry or NULL if none exists.
+ */
+const wchar_t *console_history_get_future(struct hyper_console_history_t *hist, int *length);
+
+
+/** Set the supposed next entry (text that is "on hold")
+  
+  \param hist A console history buffer or NULL.
+  \param text The new future entry or NULL.
+  \param text_length The length of the entry or -1 to indicate that \a text is a NUL-terminated string.
+ */
+void console_history_set_future(struct hyper_console_history_t *hist, const wchar_t *text, int text_length);
+
 
 /** Add an entry to the history.
 
